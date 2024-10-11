674e43b36ad7b8e2483be6c875cfc5b25ce63788
IvansyhPtra <irvanvivo53@gmail.com>
2024-10-10 20:38:59 -0700

    Add files via upload

---
+++ switch2.php
@@ -0,0 +1,20 @@
+<?php
+$nilai = 50;
+switch($niai){
+    case 90:
+        echo "Nilai anda A";
+        break;
+    case 80:
+        echo "Nilai anda B";
+        break;
+    case 70:
+        echo "Nilai anda C";
+        break;
+    case 60:
+        echo "Nilai anda D";
+         break;
+    default:
+        echo "Nilai anda E";
+        break;
+}
+?>
